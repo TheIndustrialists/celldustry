8087d3f7fa5bfe5662d61aeb6b178397d966b180
ItsTheEditor <editspace90@gmail.com>
2024-01-03 17:02:09 -0800

    Update celldustry.java

--- core/src/com/editspace/game/celldustry.java
+++ core/src/com/editspace/game/celldustry.java
@@ -1,13 +1,30 @@
 package com.editspace.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
 
 
 public class celldustry extends ApplicationAdapter {
-    public static void main (String[] arg){
+    private Texture cell;
+    private Texture protein;
+    private Sound eatsound;
+    private Music speed;
 
+    @Override
+    public void create(){
+        cell = new Texture(Gdx.files.internal("cell.png"));
+        protein = new Texture(Gdx.files.internal("protein.png"));
 
+        eatsound = new  Gdx.audio.newSound(Gdx.files.internal("eatsound.mp3"));
+        speed = new Gdx.audio.newMusic(Gdx.files.internal("speed.mp3"));
+
+        speed.setLooping(true);
+        speed.play();
     }
+
+
 
 }
